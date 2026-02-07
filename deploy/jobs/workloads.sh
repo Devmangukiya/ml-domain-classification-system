fb1fde3b88ae3c8a3f1da8d4650e5c40649b25a8
Devmangukiya <devmangukiya561@gmail.com>
2026-02-07 23:45:46 +0530

    Fix line endings and simplify entrypoint

--- deploy/jobs/workloads.sh
+++ deploy/jobs/workloads.sh
@@ -1,19 +1,6 @@
-set -ex
+#!/usr/bin/env bash
+set -e
 
-export PYTHONPATH=$PYTHONPATH:$PWD
-mkdir -p results
-
-
-# Test data
-export RESULTS_FILE=results/test_data_results.txt
-export DATASET_LOC="https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/dataset.csv"
-pytest --dataset-loc=$DATASET_LOC tests/data --verbose --disable-warnings > $RESULTS_FILE
-cat $RESULTS_FILE
-
-
-# Save to S3 
-BUCKET=model-registry-dev-mangukiya-v2
-
-aws s3 cp results/ \
-  "s3://$BUCKET/$GITHUB_USERNAME/results/" \
-  --recursive
+echo "WORKLOAD STARTED"
+pwd
+ls -R .
